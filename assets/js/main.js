18ea893bd01e40653dcb3d2884f7db3f77018205
Gimnastiar18 <ariegimnastiar18@gmail.com>
2023-10-01 18:22:28 +0700

    Web Profile - Gimnastiar
    Web profile

---
+++ assets/js/main.js
@@ -0,0 +1,133 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===Menu show===*/
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===menu hidden==*/
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+/*=============== SHADOW HEADER ===============*/
+const shadowHeader = () =>{
+    const header = document.getElementById('header')
+
+    this.scrollY >= 50 ? header.classList.add('shadow-header')
+                       : header.classList.remove('shadow-header')
+}
+window.addEventListener('scroll', shadowHeader)
+
+/*=============== EMAIL JS ===============*/
+const contactForm = document.getElementById('contact-form'),
+    contactMessage = document.getElementById('contact-message')
+
+const sendEmail = (e) => {
+    e.preventDefault()
+
+    emailjs.sendForm('service_txim29e','template_snqxy0i','#contact-form','McnPQgtBvExFgTrsV')
+    .then(() =>{
+        //show sent message
+        contactMessage.textContent = 'Message sent succesfully'
+
+        //remove message after five second
+        setTimeout(() =>{
+            contactMessage.textContent = ''
+        }, 5000)
+
+        //clear input fields
+        contactForm.requestFullscreen()
+    }, () =>{
+        //show eror message
+        contactMessage.textContent = 'Message not sent ( service eror 404 )'
+    })
+}
+
+contactForm.addEventListener('submit', sendEmail)
+/*=============== SHOW SCROLL UP ===============*/ 
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scroll.classList.add('show-scroll')
+                        : scrollUp.classList.remove('show-scroll')
+}
+
+window.addEventListener('scroll', scrollUp)
+/*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+              sectionTop = current.offsetTop - 58,
+              sectionId = current.getAttribute('id'),
+              sectionClass = document.querySelector('.nav__menu a[href*' + sectionId +']')
+        
+        if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+            sectionClass.classList.add('active-link')
+        }else{
+            sectionClass.classList.remove('active-link')
+        }
+    })
+}
+
+/*=============== DARK LIGHT THEME ===============*/ 
+
+/* THIS SECTION WILL BE UPDATE SOOON */
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'ri-sun-line'
+
+//previously selected topic (if user seleceted)
+const selecetedTheme = localStorage.getItem('selecetedTheme')
+const selecetedIcon = localStorage.getItem('selected-icon')
+
+//validating the dark theme class
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'ri-moon-line' : 'ri-sun-line'
+
+if (selecetedTheme) {
+    document.body.classList[selecetedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+    themeButton.classList[selecetedIcon === 'ri-moon-line' ? 'add' : 'remove'](iconTheme)
+}
+
+//activate / deactivate the theme manually with button
+themeButton.addEventListener('click', () => {
+    //add or remove the dark / icon theme
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
+
+/*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '60px',
+    duration: 2500,
+    delay:400,
+})
+
+sr.reveal('.home__perfil, .about__image', {origin: 'right'})
+sr.reveal('.home__name, .home__info, .about__container .section_title-1, .about_info', {origin: 'left'})
